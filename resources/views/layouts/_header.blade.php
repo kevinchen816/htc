2415fe8b52b9be3b1f175a2cf17c58ed3a9dd29a
kevinchen816 <kevin@10ware.com>
2019-03-14 07:16:47 +0000

    update portal logo #2

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -24,7 +24,7 @@
                     <h2>{{ env('APP_NAME') }}</h2>
 @elseif (env('APP_REGION') == 'tw')
                     <!-- <img class="main-logo" src="{{ route('home') }}/images/logo_eztoview.png" alt="logo" /> -->
-                    <img class="main-logo" height="60" src="{{ route('home') }}/images/logo_eztoview.png" alt="logo" />
+                    <img class="main-logo" height="50" src="{{ route('home') }}/images/logo_eztoview.png" alt="logo" />
                     <!-- <img class="main-logo" height="60" src="{{ route('home') }}/images/logo_eztoview-2.bmp" alt="logo" /> -->
                     <!-- <h2>{{ env('APP_NAME') }}</h2> -->
 @elseif (env('APP_REGION') == 'cn')
