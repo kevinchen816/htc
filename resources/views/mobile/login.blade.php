c206928e64522595957f1d8a771672f819cce2d2
kevinchen816 <kevin@10ware.com>
2019-05-12 12:32:27 +0000

    add APP_MOBILE_EX=true

--- resources/views/mobile/login.blade.php
+++ resources/views/mobile/login.blade.php
@@ -51,6 +51,31 @@
 @endif
                         <button type="submit" class="btn btn-primary">{{ trans('htc.Login') }}</button>
                     </form>
+
+@if (env('APP_MOBILE_EX'))
+<hr>
+
+<!-- <p>
+    Need to Register for a new account? <br/>
+
+        <a class="btn btn-primary" href="{{ route('register') }}" role="button">Register</a>
+</p> -->
+
+<p>
+    {{ trans('htc.dont_you_have_an_account') }}
+    <a href="{{ route('register') }}"> {{ trans('htc.Create New Account') }}</a>
+</p>
+
+<p>
+    {{ trans('htc.did_you') }} <a href="{{ route('password.request') }}">{{ trans('htc.Forget Your Password') }}</a>
+</p>
+
+<p>
+    {{ trans('htc.you_already_have_an_account_an_need_to') }}
+    <a href="{{ route('confirm.send') }}">{{ trans('htc.Resend Confirmation Email') }}</a>
+</p>
+@endif
+
                 </div>
             </div>
 @if (0)
