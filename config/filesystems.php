e77045eb14e2d0fa440392fb3bb39cc116a750a8
Kevin Chen <kevin@10ware.com>
2019-05-14 20:37:01 +0000

    update .env

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,10 +65,10 @@ return [
 
         'oss' => [
             'driver'        => 'oss',
-            'access_id'     => env('OSS_ACCESS_KEY_ID'),                  // <Your Aliyun OSS AccessKeyId>
-            'access_key'    => env('OSS_ACCESS_KEY_SECRET'),    // <Your Aliyun OSS AccessKeySecret>
-            'bucket'        => env('OSS_BUCKET_NAME'),                            // <OSS bucket name>
-            'endpoint'      => 'oss-cn-shenzhen.aliyuncs.com',       // OSS 外网节点或自定义外部域名 <the endpoint of OSS, E.g: oss-cn-hangzhou.aliyuncs.com | custom domain, E.g:img.abc.com>
+            'access_id'     => env('OSS_ACCESS_KEY_ID'),    // <Your Aliyun OSS AccessKeyId>
+            'access_key'    => env('OSS_ACCESS_KEY_SECRET'),// <Your Aliyun OSS AccessKeySecret>
+            'bucket'        => env('OSS_BUCKET_NAME'),      // <OSS bucket name>
+            'endpoint'      => env('OSS_ENDPOINT'),         // OSS 外网节点或自定义外部域名 <the endpoint of OSS, E.g: oss-cn-hangzhou.aliyuncs.com | custom domain, E.g:img.abc.com>
             //'endpoint_internal' => '<internal endpoint [OSS内网节点] 如：oss-cn-shenzhen-internal.aliyuncs.com>', // v2.0.4 新增配置属性，如果为空，则默认使用 endpoint 配置(由于内网上传有点小问题未解决，请大家暂时不要使用内网节点上传，正在与阿里技术沟通中)
             // 'cdnDomain'     => '',      // 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn <CDN domain, cdn域名>
             'ssl'           => true,    // true use https | false use http
